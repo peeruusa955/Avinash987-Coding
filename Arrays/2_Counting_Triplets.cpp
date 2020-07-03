2db7fa771c39582a4a76dd7158a107997acea103
Lalith Avinash <avinash.lalith97@gmail.com>
2020-07-03 18:19:04 +0530

    Non Optimized solution for Counting Triplets

--- Arrays/2_Counting_Triplets.cpp
+++ Arrays/2_Counting_Triplets.cpp
@@ -6,21 +6,55 @@ First line of each test case contains an Integer N denoting size of array and th
 
 Output:
 For each test case, print the count of all triplets, in new line. If no such triplets can form, print "-1". */
+#include<iostream>
+#include<algorithm> // std::sort
+using namespace std;
+
+//Logic
+void CountTriplets(int array[], int N)
+{
+    int count = 0;
+    if(N <= 2)
+        cout<< -1 <<endl;
+
+    sort(array, array+N);
+
+    for (int i = N - 1; i >= 0; i--)
+    {
+        int j = 0;
+        int k = i - 1;
+
+        while (j < k)
+        {
+            if (array[i] == array[j] + array[k])
+                count += 1;
+            else if (array[i] > array[j] + array[k])
+                j += 1;
+            else
+                k -= 1;
+        }
+    }
+    if(count == 0)
+        count = -1;
+
+    cout<< count <<endl;
+    return;
+}
 
 int main(void)
 {
-    int T, N, S;
+    int T, N;
     cin >> T;
     // For all the test cases
     for(int t=0; t<T; t++)
     {
-        cin >> N >> S;
+        cin >> N;
         int array[N];
         // Take the array input rrom the user
         for(int i=0; i<N; i++)
             cin >> array[i];
 
-        // SubArraySum(array, N, S);
+        CountTriplets(array, N);
 
     }
     return 0;
