5f4d82f30902a57fbce7bf6b3e6a2617beb9363c
Lalith Avinash <avinash.lalith97@gmail.com>
2020-07-06 20:30:19 +0530

    Modified cpp and py geeksforgeeks templates

--- Arrays/gfg_template.py
+++ Arrays/gfg_template.py
@@ -1,6 +1,8 @@
 """
 Geeks for Geeks Problems Solution Template Python
 """
+def function_name():
+    pass
 
 if __name__ == "__main__":
     t = int(input())
@@ -8,5 +10,5 @@ if __name__ == "__main__":
         n = int(input())
         l = list(map(int, input().split()))
         k = int(input())
-        result = function_name(args)
+        result = function_name()
         print(result)
