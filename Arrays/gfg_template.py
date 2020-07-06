06c74290e02b25923b4a3322e9f0df7fe9c59d3d
Lalith Avinash <avinash.lalith97@gmail.com>
2020-07-06 19:00:48 +0530

    Added cpp and py geeksforgeeks templates

---
+++ Arrays/gfg_template.py
@@ -0,0 +1,12 @@
+"""
+Geeks for Geeks Problems Solution Template Python
+"""
+
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        l = list(map(int, input().split()))
+        k = int(input())
+        result = function_name(args)
+        print(result)
