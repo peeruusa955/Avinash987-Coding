eb5f2d34e92bc103d8fb376bc9c82c8cc80bea8e
Lalith Avinash <avinash.lalith97@gmail.com>
2020-07-10 21:23:28 +0530

    8 **Min Insertion to form a Palindrome solved in O(n^2)

--- Strings/8_Form_Palindrome.py
+++ Strings/8_Form_Palindrome.py
@@ -6,16 +6,36 @@ ab: Number of insertions required is 1. bab or aba
 aa: Number of insertions required is 0. aa
 abcd: Number of insertions required is 3. dcbabcd
 """
-
-def check_palindrome(string):
-    if string == string[::-1]:
-        return True
-    else:
-        return False
-
-def toFormPalindrome(s):
-
-
+# Reuse the longest Palindrome from the Problem 2
+def longestPalindrome(s):
+    n = len(s)
+    best_len = 0
+    best_s = ""
+    for mid in range(n):
+        x = 0
+        while mid - x >= 0 and mid + x < n :
+            if s[ mid - x] != s[mid + x]:
+                break
+            sub_len = 2*x + 1
+            if sub_len > best_len:
+                best_len = sub_len
+                best_s = s[mid-x: mid+x + 1]
+            x += 1
+    for mid in range(n-1):
+        x = 1
+        while mid - x + 1 >=0 and mid + x < n :
+            if s[mid-x+1] != s[mid+x]:
+                break
+            sub_len = 2 * x
+            if sub_len > best_len:
+                best_len = sub_len
+                best_s = s[mid-x+1: mid+x+1]
+            x += 1
+    return best_s
+def toFormPalindrome(string):
+    best_substring = longestPalindrome(string[::-1])
+    min_insertions = len(string) - len(best_substring)
+    return min_insertions
 
 if __name__ == "__main__":
     t = int(input())
