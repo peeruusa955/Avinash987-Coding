106f2f6aaafd8a05b29e391e0221306c7dfb86ce
Lalith Avinash <avinash.lalith97@gmail.com>
2020-07-16 11:45:35 +0530

    1 Reverse the linked list

--- Linked Lists/1_Reverse.cpp
+++ Linked Lists/1_Reverse.cpp
@@ -1,10 +1,10 @@
 // Given a linked list of N nodes. The task is to reverse this list.
-
+// Also look into geeksforgeeks recursion method
 // Should reverse list and return new head.
 struct Node* reverseList(struct Node *head)
 {
     // code here
-    if(head==NULL)
+    if(head==NULL || head->next == NULL)
         return head;
     else
     {
